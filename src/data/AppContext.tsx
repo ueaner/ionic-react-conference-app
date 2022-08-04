b8a087e2e17a216422cb2ca061d923cb8e793498
ueaner <ueaner@gmail.com>
2022-08-04 23:57:26 +0800

    vite

--- src/data/AppContext.tsx
+++ src/data/AppContext.tsx
@@ -1,26 +1,33 @@
-import React, { createContext, useReducer } from 'react';
-import { initialState, AppState, reducers } from './state'
+import React, { createContext, useReducer } from "react"
+import { initialState, AppState, reducers } from "./state"
 
 export interface AppContextState {
-  state: AppState;
-  dispatch: React.Dispatch<any>;
+	state: AppState
+	dispatch: React.Dispatch<any>
 }
 
 export const AppContext = createContext<AppContextState>({
-  state: initialState,
-  dispatch: () => undefined
-});
+	state: initialState,
+	dispatch: () => undefined,
+})
 
-export const AppContextProvider: React.FC = (props => {
+type AppContextProviderProps = {
+	children: React.ReactNode
+}
 
-  const [store, dispatch] = useReducer(reducers, initialState);
+export const AppContextProvider: React.FC<AppContextProviderProps> = (
+	props: AppContextProviderProps
+) => {
+	const [store, dispatch] = useReducer(reducers, initialState)
 
-  return (
-    <AppContext.Provider value={{
-      state: store,
-      dispatch
-    }}>
-      {props.children}
-    </AppContext.Provider>
-  )
-});
+	return (
+		<AppContext.Provider
+			value={{
+				state: store,
+				dispatch,
+			}}
+		>
+			{props.children}
+		</AppContext.Provider>
+	)
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
+
+// // react v18
+// import React from "react"
+// import ReactDOM from "react-dom/client"
+// import App from "./App"
+// //import './index.css'
+//
+// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+// 	<React.StrictMode>
+// 		<App />
+// 	</React.StrictMode>
+// )
